3aed946faefecadb53b11d2530cf92ee47f82aff
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-02-12 03:28:18 +0000

    Add implementation for free_player and player_info

--- game.c
+++ game.c
@@ -32,3 +32,37 @@ player_t init_player(const char* name) {
 
         return p;
 }
+
+void free_player(player_t* player) {
+        free_hand(player->hand);
+        player->hand = NULL;
+
+        free((char*)player->name);
+        player->name = NULL;
+
+        free(player);
+
+        return
+}
+
+const char* player_info(player_t* player) {
+        char*           buffer;
+        const char*     hinfo;
+        const char*     name;
+        size_t          buff_len;
+
+        name = player->name;
+        hinfo = hand_info(player->hand);
+
+        // +11 for additional hardcoded string sections
+        buff_len = strlen(name) + strlen(hinfo) + 1 + 11;
+
+        buffer = (char*)malloc(bufflen);
+        if (buffer == NULL) {
+                return NULL;
+        }
+
+        snprintf(buffer, buff_len, "Player: %s | %s", name, hinfo)
+
+        return buffer;
+} 
