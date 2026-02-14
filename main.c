3a0937acec9e36afa0f06f317dfc6d0b6a3565e2
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-02-14 00:41:21 +0000

    Update main

--- main.c
+++ main.c
@@ -6,28 +6,29 @@
 
 int main() {
         game_t*         game;
-        deck_t*         deck;
+        cards_t*        deck;
         player_t*       players[4];
+        const char*     gi;
 
         printf("Hello Bridge Players!\n");
         
-        deck = init_deck();
-        shuffle_deck(deck);
-        for(size_t i = 0; i < B_STD_NUM_PLAYERS; ++i) {
+        deck = init_card_collection(B_STD_DECK_SIZE);
+        create_deck(deck);
+        shuffle(deck);
+        for (size_t i = 0; i < B_STD_NUM_PLAYERS; ++i) {
                 player_t* p;
 
                 p = init_player("Example");
                 players[i] = p;
         }
 
+        printf("Let's Begin!\n");
         game = init_game(players, deck);
-        
-        free_deck(deck);
-        for(size_t i = 0; i < B_STD_NUM_PLAYERS; ++i) {
-                free(players[i]);
-                players[i] = NULL;
-        }
-        deck = NULL;
+        gi = game_info(game); 
+        printf("%s\n", gi);
+       
+        free_game(game);
+        game = NULL;
 
         return 0;
 }
