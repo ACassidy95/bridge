797121147cebba62335fd1541e9577cdf446c8a9
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-02-16 00:04:46 +0000

    Add files to contain general utility functions to cut down duplications

---
+++ utils.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+const char* get_user_input(const char* template) {
+        
+}
+
+void quit(const char* err) {
+        fprintf(stderr, "%s\n", err);
+        exit(1);
+}
+
+input_error:
---
+++ utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+const char*     get_user_input(const char*);
+void            quit(const char*);
+
+#endif
+
